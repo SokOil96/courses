1a6c9fdb823a32ea5dcd262171ea168bbf65dca8
Slava <wadehot13@gmail.com>
2022-11-13 23:56:45 +0600

    commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+// const result = confirm("Выучу ли я JS?");
+// console.log(result); 
+
+// const answer = prompt("У меня есть мотивация?", "");
+// console.log(answer);
+
+// const answers = [];
+// answers [0] = prompt("WHat?", "");
+// answers [1] = prompt("Where?", "");
+// answers [2] = prompt("Who?", "");
+// answers [3] = prompt("Why?", "");
+// console.log(answers);
+// console.log(3*4 == 12)
